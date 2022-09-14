414a3580daea9446a315046241106198b72d7b84
Kunal Pandharkar <63575815+KunalPandharkar@users.noreply.github.com>
2022-09-14 22:52:43 +0530

    Add files via upload

--- EscapeCodes.cpp
+++ EscapeCodes.cpp
@@ -1,23 +1,10 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main()
-{
-    cout << "one\ntwo\nthree" << endl;
-    cout << "left\tright" << endl;
-    cout << "vertical tab \v vertical tab" << endl;
-    cout << "\" , \' , \\";
+int main(){
+	cout << "first line \nsecond line \n";
 
-    // raw string avoid escape characters
-    string str = R"(string with \backslash)";
-    string str1 = "&%$(string with \\backslash)&%$";
+	cout << "left \t right" << endl;
 
-    cout << endl
-         << str << " " << str1;
-
-    int a = 10, b;
-
-    int c = (b=10,b+10);
-
-    cout << endl << c ;
+	cout << "\v vertical tab";
 }
